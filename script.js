0c910e46803efbf5d348cd17e48c83d43c800217
k4r0lina <104777013+k4r0lina@users.noreply.github.com>
2024-06-06 13:46:24 -0300

    sss

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                afirmacao: "No início ficou com medo dessa responsabilidade. "
             },
             {
                 texto: "Isso é maravilhoso!",
