24293871fca5637b8e42cbef59a14a318694dc92
k4r0lina <104777013+k4r0lina@users.noreply.github.com>
2024-06-27 09:07:56 -0300

    aaaaaaa

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ const perguntas = [
         enunciado: " Imagina que você é um presidente, e o congresso te apresenta dois projetos de leis. Uma é para diminuir as emissões de gases poluentes com o custos de que algumas empresas irão sair do país, a segunda lei é para aumentar o limite dos gases poluentes e com isso terá mais empresas ",
         alternativas: [
             {
-                texto: "aprovar a primeira lei",
-                afirmacao: "ter um país com menos gases poluentes. "
+                texto: "Aprovar a primeira lei",
+                afirmacao: "Ter um país com menos gases poluentes. "
             },
             {
-                texto: "aprovar a segunda lei",
+                texto: "Aprovar a segunda lei",
                 afirmacao: "Ter mais gases oluentes por ter mais empresas ."
             }
         ]
@@ -22,12 +22,12 @@ const perguntas = [
         enunciado: "As empresas de carros querem fabricar mais veículos, com isso você tem que aprovar os carros elétricos/biocombustíveis que precisará de uma verba maior para a fabricação ou carros com gases poluentes que será usado menos verba para a sua fabricação. Qual você irá aprovar?",
         alternativas: [
             {
-                texto: "carros elétricos/biocombustíveis "
-                afirmacao: "você escolheu um carro mais sustentável para o meio ambiente, mas a verba será maior para a fabricação."
+                texto: "Carros elétricos/biocombustíveis "
+                afirmacao: "Você escolheu um carro mais sustentável para o meio ambiente, mas a verba será maior para a fabricação."
             },
             {
-                texto: "carros com gases poluentes.",
-                afirmacao: "você escolheu o carro com menos verba para a fabricação, mas ele é mais poluente."
+                texto: "Carros com gases poluentes.",
+                afirmacao: "Você escolheu o carro com menos verba para a fabricação, mas ele é mais poluente."
             }
         ]
     },
@@ -35,11 +35,11 @@ const perguntas = [
         enunciado: "O congresso te apresenta mais duas leis, sendo elas usar mais energias limpas que você poderá colocar placas solares pelos campos e essa energia ser utilizada de noite nas ruas ou  liberar uma verba para uma pesquisa a energia de fusão nuclear tendo 50% que poderá dar certo e 50% que pode dar errado causando uma explosão nuclear",
         alternativas: [
             {
-                texto: "aprova a primeira opção.",
-                afirmacao: "colocar placas solares pelos campos e usar sua energia nas cidades de noite."
+                texto: "Aprova a primeira opção.",
+                afirmacao: "Colocar placas solares pelos campos e usar sua energia nas cidades de noite."
             },
             {
-                texto: "aprova a segunda opção.",
+                texto: "Aprova a segunda opção.",
                 afirmacao: "Dar o dinheiro para a pesquisa sobre energias de fusão nuclear."
             }
         ]
@@ -48,12 +48,12 @@ const perguntas = [
         enunciado: "A partir de agora temos mais água em todo mundo, pois nossa água potável não é mais utilizada para gerar energia nas usinas hidrelétricas. Com isso o congresso te apresenta dois novos projetos de leis, são elas: Diminuir o uso inapropriado  da água, assim gerando uma multa para quem utilizar a água de forma inadequada. Ou abaixar o preço do uso da água.",
         alternativas: [
             {
-                texto: "aprovar o primeiro projeto .",
-                afirmacao: " utilizar a água da forma certa, fazendo o uso correto e adequado da água."
+                texto: "Aprovar o primeiro projeto .",
+                afirmacao: " Utilizar a água da forma certa, fazendo o uso correto e adequado da água."
             },
             {
-                texto: "aprovar o segundo projeto.",
-                afirmacao: " a baixar o valor do preço utilizado pela água"
+                texto: "Aprovar o segundo projeto.",
+                afirmacao: "A baixar o valor do preço utilizado pela água"
             }
         ]
     },
@@ -61,12 +61,12 @@ const perguntas = [
         enunciado: "O último projeto que foi criado possibilitou o maior uso de água, com isso o congresso apresentou mais dois projetos de leis, assim estabelecendo eles com base no meio ambiente, primeiro projeto: fazer o plantio de mais árvores pelas cidades, possibilitando a melhora da qualidade do ar. OU o segundo projeto, onde ele apresenta pegar a verba, e fazer para uso de novas pavimentações na cidade",
         alternativas: [
             {
-                texto: "aprovar o primeiro projeto.",
-                afirmacao: " plantar mais árvores para melhorar a qualidade do ar."
+                texto: "Aprovar o primeiro projeto.",
+                afirmacao: " Plantar mais árvores para melhorar a qualidade do ar."
             },
             {
-                texto: "aprovar segundo projeto",
-                afirmacao: "fazer novas pavimentações pela cidade. "
+                texto: "Aprovar segundo projeto",
+                afirmacao: "Fazer novas pavimentações pela cidade. "
             }
         ]
     },
